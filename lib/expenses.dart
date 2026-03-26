bc3a03d925fa91893193bb3de4d8c80c0b1a98ec
Jaden Mckain <jaden12opurum@gmail.com>
2026-03-26 11:46:46 -0400

    made dummy data in the expenses file. march 26th.

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jaden_expensetracker_march24/models/expense.dart';
 
 class Expenses extends StatefulWidget{
   const Expenses({super.key});
@@ -8,6 +9,12 @@ class Expenses extends StatefulWidget{
   }
 }
 class _ExpensesState extends State<Expenses>{
+  final List<Expense> _registeredExpenses = [
+    Expense(title: "Amores Pizza Cafe", amount: 15.94, date: DateTime.now(), category: Category.food),
+    Expense(title: "Whole Foods Market", amount: 22.73, date: DateTime.now(), category: Category.food),
+    Expense(title: "Long Island Rail Road", amount: 106.99, date: DateTime.now(), category: Category.travel),
+    Expense(title: "Staples", amount: 8.34, date: DateTime.now(), category: Category.work)
+  ];
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,9 +1,11 @@
 import 'package:uuid/uuid.dart';
 const uuid = Uuid();
+enum Category {travel, leisure, work, food}
 class Expense {
-  Expense({required this.title, required this.amount, required this.date}) : id = uuid.v4();
+  Expense({required this.title, required this.amount, required this.date, required this.category}) : id = uuid.v4();
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
