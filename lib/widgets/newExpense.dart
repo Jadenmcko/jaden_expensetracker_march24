35ead8e5378a15e3f9c56fdeafc8f5e7377435d1
Jaden Mckain <jaden12opurum@gmail.com>
2026-04-02 12:08:17 -0400

    We added the amount in the New Expense file, along with the amount Controller function. April 2nd.

--- lib/widgets/newExpense.dart
+++ lib/widgets/newExpense.dart
@@ -9,18 +9,23 @@ class Newexpense extends StatefulWidget{
 }
 class _NewexpenseState extends State<Newexpense>{
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Padding(padding: EdgeInsets.all(16),
     child: Column(children: [
-      TextField(controller: _titleController,maxLength: 50, keyboardType: TextInputType.name, decoration: InputDecoration(
+      TextField(controller: _titleController,
+      maxLength: 50, keyboardType: TextInputType.name, decoration: InputDecoration(
         label: Text("Ttitle"),),
         ),
-        Row(children: [
-          //
-        ]),
-        Row(children: [ElevatedButton(onPressed: (){print(_titleController.text);
+  
+          // amount field
+          TextField(controller: _amountController,
+          maxLength: 10, keyboardType: TextInputType.number, decoration: InputDecoration(
+            prefixText: '\$', label: Text("Amount")),),
+     
+        Row(children: [ElevatedButton(onPressed: (){print(_titleController.text); print(_amountController);
         }, child: Text("Save Your Expense"))],)
       ]),
     );
